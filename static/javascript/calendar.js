5812111e8462ff15f545bee7aaaa46fa70d52600
gys <gys@gyss-MacBook-Pro.local>
2019-11-13 00:31:25 +0800

    _

--- static/javascript/calendar.js
+++ static/javascript/calendar.js
@@ -42,26 +42,26 @@
         },
     ];
     let events = [   //#ffd233   //#ff6633  #bd302a  #356930
-        {
-            id: "2",
-            resourceId: "a",
-            title: "天窗*1",
-            color: "#da5e58",
-            num: 1,
-            start: "2019-11-07T00:00:00",
-            end: "2019-11-07T08:00:00",
-            classNames: ["event-class"],
-        },
-        {
-            id: "3",
-            resourceId: "a",
-            title: "侧围*4",
-            color: "#47c7dc",
-            num: 2,
-            start: "2019-11-07T00:00:00",
-            end: "2019-11-07T06:00:00",
-            classNames: ["event-class"]
-        }
+        // {
+        //     id: "2",
+        //     resourceId: "a",
+        //     title: "天窗*1",
+        //     color: "#da5e58",
+        //     num: 1,
+        //     start: "2019-11-07T00:00:00",
+        //     end: "2019-11-07T08:00:00",
+        //     classNames: ["event-class"],
+        // },
+        // {
+        //     id: "3",
+        //     resourceId: "a",
+        //     title: "侧围*4",
+        //     color: "#47c7dc",
+        //     num: 2,
+        //     start: "2019-11-07T00:00:00",
+        //     end: "2019-11-07T06:00:00",
+        //     classNames: ["event-class"]
+        // }
     ];
     let customButtons = {
         changeBtn: {
@@ -103,22 +103,42 @@
     };
 
     /**
+     * @param value
+     * @returns {string}
+     */
+    function getColor(value) {
+
+        let colors = [
+            '#57c1ff',
+            '#79bf73',
+            '#da5e58'
+        ];
+        if (value > (colors.length -1)) {
+            value = 0;
+        }
+        return colors[value];
+    }
+
+    /**
      * 设置工作台列表
      */
     function getAllPlatform(callback) {
         $lib.http('/apis/resource/GetAllPlatform', {}, function (res) {
-            console.log(res);
-            // let temp = [];
-            for (let i = 0; i < res.length; i++) {
-                let id = resources[i].id;
-                let label = resources[i].title;
-                let option = '<option value="'+id+'">'+label+'</option>';
-                $('#selectRobotId').append(option);
-                res[i].id = res[i].ID;
-                res[i].title = res[i].Name;
+            let temp = [];
+            if (res.Data) {
+                for (let i = 0; i < res.Data.length; i++) {
+                    let item = res.Data[i];
+                    item['id'] = item['ID'];
+                    item['title'] = item['Name'];
+                    let id = item.id;
+                    let label = item.title;
+                    let option = '<option value="'+id+'">'+label+'</option>';
+                    $('#selectRobotId').append(option);
+                    temp.push(item)
 
+                }
             }
-            resources = res;
+            resources = temp;
             if (callback) {
                 callback();
             }
@@ -161,13 +181,15 @@
 
         calendar = new FullCalendar.Calendar(calendarContainer, {plugins: ['resourceTimeline', 'interaction'],
             defaultView: 'calendarTimeLine',
+            defaultDate: new Date(),
+            nowIndicator: true,
             views: {
                 calendarTimeLine: {
                     type: "resourceTimeline",
                     titleName: "日历图",
                     slotWidth: 225,
                     slotLabelFormat: function (date) {
-                        return date.date.month + '月' + date.date.day + '日';
+                        return date.date.month + 1 + '月' + date.date.day + '日';
                     },
                     slotDuration: "24:00:00",
                     resourceLabelText: "工作台",
@@ -178,7 +200,7 @@
                     }
                 }
             },
-            locale: "zh-cn",
+            // locale: "zh-cn",
             height: "parent",
             droppable: true,
             editable: true,
@@ -193,19 +215,19 @@
             customButtons: customButtons,
             header: { left: '', center: 'title', right: 'today prev,next' },
             events: function(date, callback1, callback2){
+                console.log(date);
                 console.log(date.startStr + ' ----> ' + date.endStr);
                 $lib.http('/apis/inspectorder/GetSubInspectOrderDateByTime', {start: date.startStr, stop: date.endStr}, function (res) {
-                    console.log(res)
                     let temp = [];
-                    for (let i = 0; i < res.length; i++) {
+                    for (let i = 0; i < res.Data.length; i++) {
                         let item = {
-                            id: res[i]['Device_id'],
-                            resourceId: res[i]['Platform_id'],
-                            title: res[i]['PartName'],
-                            color: "#da5e58",
+                            id: res.Data[i]['Device_id'],
+                            resourceId: res.Data[i]['Platform_id'],
+                            title: res.Data[i]['PartName'],
+                            color: getColor(res.Data[i]['Device_id']),
                             num: 1,
-                            start: res[i]['Plan_Start_Time'],
-                            end: res[i]['Plan_End_Time'],
+                            start: $lib.dateFormat('YYYY-mm-dd HH:MM:SS', new Date(res.Data[i]['Plan_Start_Time'])),
+                            end: $lib.dateFormat('YYYY-mm-dd HH:MM:SS', new Date(res.Data[i]['Plan_End_Time'])),
                             classNames: ["event-class"],
                         };
                         temp.push(item);
